3d236d256a75ac8898d1728fd24a9d4168f8b5d0
ALoic <loic.rudolph@web.de>
2021-03-30 23:10:41 +0200

    Anpassung 'fAverageTotalSize'

--- Tabelle1/fAverageTotalSize.m
+++ Tabelle1/fAverageTotalSize.m
@@ -1,24 +1,20 @@
-function [dAverageTotalSize] = fAverageTotalSize(currentCountryStructure)
+function [dAverageTotalSize] = fAverageTotalSize(mMVall)
 %% AVERAGE TOTAL SIZE
 % Calculating the average total size of a country using the market value.
-% All market values of a country are added to a total size and the mean of
-% all firms of a country is calculated.
+% All market values of the firms of a country per month are added to a
+% total size and the mean is calculated.
 %% REQUIRES
 % struct with the full data of the current country
 %% RETURNS
 % double with the average total size of the country
 %% FUNCTION
-% determining the country firm names
-fns = fieldnames(currentCountryStructure);
-% for loop iterating through each firm
-for i = 1:numel(currentCountryStructure)
-% creating an empty cell array
-   marketvalues = {};
-% adding the market values to the cell array
-   marketvalues = [marketvalues; currentCountryStructure.(fns{i}).MARKET_VALUE];   
-end
-% convert cell array into matrix, calculating means while omitting NANs and
-% rounding result to a whole number
-dAverageTotalSize = round(mean(cell2mat(marketvalues),'omitnan'));
+% turning NANs of mMVall into zeros
+mMVall(isnan(mMVall))=0;
+% adding the market value of each firm to the countries total market value
+% per month
+vTotalMV = sum(mMVall,2);
+% calculating the mean of the total market values and rounding to a full
+% number
+dAverageTotalSize = round(mean(vTotalMV));
 end
 

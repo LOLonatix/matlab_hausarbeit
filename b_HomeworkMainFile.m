bd295991da7a0702d133926beef3c3340e560a06
JSchmidt562 <jonasschmidt98.js@gmail.com>
2021-04-21 21:42:57 +0200

    Regression als Funktion in Mainfile eingegliedert

--- b_HomeworkMainFile.m
+++ b_HomeworkMainFile.m
@@ -14,7 +14,7 @@ addpath(genpath('folder_MainFiles'));
 % countries data separatedly by using functions from
 % 'folder_FunctionsFilteringData'. Afterwards, it saves them in
 % 'folder_FilteredData'.
-b_FilterData;
+%b_FilterData;
 
 %% Afterwards the function fCreateTable1 reproduces table 1 from the given paper
 % Likewise, more functions from 'folder_Tabelle1' are used. The table is
@@ -26,9 +26,9 @@ b_FilterData;
 %% The second table is created like the first one..
 % Likewise, more functions from 'folder_Tabelle2' are used. The table is
 % saved as a xlsx.
-tTable2 = fCreateTable2();
+%tTable2 = fCreateTable2();
 %writetable(tTable2, 'Tabelle2.xlsx');
 
 
 %% Finally, a regression is created by the function fCreateRegression
-%regression = fCreateRegression();
+[tTable_Regression1, tTable_Regression2, tTable_Regression3] = fCreateRegression();
